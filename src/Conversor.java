b3b98e4c919d89522260b5bd5dfdafdbdc04ab04
David Herrera <dsherrera650@soy.sena.edu.co~>
2024-10-27 00:26:41 -0500

    conversor 1.0

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,9 +1,10 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Conversor {
     private int opcion = 0;
+    List<String> historial = new ArrayList<>();
 
     public void convertir() {
         Scanner teclado = new Scanner(System.in);
@@ -17,32 +18,51 @@ public class Conversor {
         conversiones.put(6, new String[]{"COP", "USD"});
         conversiones.put(7, new String[]{"USD", "ARS"});
         conversiones.put(8, new String[]{"ARS", "USD"});
+        conversiones.put(9, new String[]{"USD", "BOB"});
+        conversiones.put(10, new String[]{"BOB", "USD"});
+        conversiones.put(11, new String[]{"USD", "CLP"});
+        conversiones.put(12, new String[]{"CLP", "USD"});
         while (opcion != 9) {
             System.out.println("""
                     Bienvenido al conversor de monedas
-                    1. Dolar a Real Brasileño
-                    2. Real Brasileño a Dolar
-                    3. Dolar a Peso Mexicano
-                    4. Peso Mexicano a Dolar
-                    5. Dolar a Peso Colombiano
-                    6. Peso Colombiano a Dolar
-                    7. Dolar a Peso Argentino
-                    8. Peso Argentino a Dolar
-                    9. Para Finalizar el programa
-                    ** Elige una opcion para continuar **
+                    1. Dólar a real brasileño
+                    2. Real brasileño a dólar
+                    3. Dólar a peso mexicano
+                    4. Peso mexicano a dólar
+                    5. Dólar a peso colombiano
+                    6. Peso colombiano a dólar
+                    7. Dólar a peso argentino
+                    8. Peso argentino a dólar                    
+                    9. Dólar a boliviano
+                    10. Boliviano a dólar
+                    11. Dólar a peso chileno
+                    12. Peso chileno a dólar
+                    13. Para visualizar el historial
+                    14. Para finalizar el programa
+                    ** Elige una opción para continuar **
                     """);
             opcion = teclado.nextInt();
-            if (opcion > 0 && opcion < 9) {
+            if (opcion > 0 && opcion < 13) {
                 System.out.println("Digite el valor que desea convertir");
                 double cantidad = teclado.nextDouble();
                 String[] conversion = conversiones.get(opcion);
                 Moneda moneda = http.api(conversion[0], conversion[1], cantidad);
                 System.out.println(moneda);
-            } else if (opcion == 9) {
+                LocalDateTime tiempo = LocalDateTime.now();
+                DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                historial.add("Moneda de cambio: " + conversion[0] + " a " + "moneda a cambiar: " + conversion[1] + "\n" +
+                        "Valor de la moneda de cambio: " + cantidad + " valor de la conversión: " + moneda.conversion_result() + "\n" +
+                        "Fecha y hora que se realizó la conversión: " + tiempo.format(formato) + "\n");
+            } else if (opcion == 13) {
+                System.out.println("Historial de las conversiones de monedas que ha realizado:\n");
+                for (String datos : historial) {
+                    System.out.println(datos);
+                }
+            } else if (opcion == 14) {
                 System.out.println("El programa va a finalizar");
                 break;
             } else {
-                System.out.println("Opcion no valida");
+                System.out.println("Opción no válida");
                 break;
             }
         }
